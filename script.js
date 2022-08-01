78328382689fc2ce6ed4479c8c7335c8c0cab523
jmndz <janaemilyn@gmail.com>
2022-08-01 15:03:54 +0800

    Numbers display when clicked/pressed

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+const calcButton = document.querySelectorAll('.calc-btn');
+window.addEventListener('keydown', pressNum);
+let storedInput = document.querySelector('.input-1');
+let currentInput = document.querySelector('.input-2');
+storedInput.textContent = '';
+currentInput.textContent = "0";
+calcButton.forEach(button => button.addEventListener('click',function(){
+    clickNum(button);
+}));
+
+function clickNum(btn){
+    if(currentInput.textContent === "0"){
+        currentInput.textContent = btn.textContent;
+    }else{
+        if(btn.textContent == 0 || btn.textContent == 1 || btn.textContent == 2 || btn.textContent == 3 ||
+            btn.textContent == 4 || btn.textContent == 5 || btn.textContent == 6 || btn.textContent == 7 ||
+            btn.textContent == 8 || btn.textContent == 9){
+                currentInput.textContent += btn.textContent;
+        }
+    }
+    
+}
+
+function pressNum(e){
+    const btn = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    if(!btn){
+        return;
+    }else{
+        if(currentInput.textContent === "0"){
+            currentInput.textContent = btn.textContent;
+            console.log(currentInput.textContent+ " " +btn.textContent);
+        }else{
+            if(btn.textContent == 0 || btn.textContent == 1 || btn.textContent == 2 || btn.textContent == 3 ||
+                btn.textContent == 4 || btn.textContent == 5 || btn.textContent == 6 || btn.textContent == 7 ||
+                btn.textContent == 8 || btn.textContent == 9){
+                    currentInput.textContent += btn.textContent;
+            }
+        }
+    }
+    
+}
+
 function operate(){
     for(;;){
         let operation = prompt("Choose operation: add, subtract, multiply, divide", '');
